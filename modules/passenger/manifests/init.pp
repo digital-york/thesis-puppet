1a8b47b077a985b34d5137ba765296c1c2e5a2f7
yf508 <y.frank.feng@gmail.com>
2015-06-05 17:08:39 +0100

    first draft version of passenger+apache configration

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -1,5 +1,16 @@
 class passenger { 
   #notice("Installing Passenger ... ")
+  
+  # required for passenger gem 
+  package { "apache2-prefork-dev": 
+    ensure => installed 
+  } 
+  # required for passenger-apache-module 
+  package { "libcurl4-openssl-dev": 
+    ensure => installed 
+  }   
+  
+  
   package { 'rake':
     #ensure => latest,
     ensure => '10.4.2',
@@ -15,6 +26,36 @@ class passenger {
     ensure => '5.0.8',
     provider => 'gem',
   }
+  
+  exec { "passenger-install-apache2-module --auto": 
+    user => root, 
+    group => root, 
+    alias => "passenger_apache_module", 
+    require => Package["apache2-prefork-dev","libcurl4-openssl-dev", "passenger"], 
+    before => File["passenger_conf"] 
+  } 
+  file { "/etc/apache2/mods-available/passenger.conf": 
+    mode => "644", 
+    owner => root, 
+    group => root, 
+    alias => "passenger_conf", 
+    source => 'puppet:///modules/passenger/passenger.conf', 
+    before => File["passenger_load"] 
+  } 
+  file { "/etc/apache2/mods-available/passenger.load": 
+    mode => "644", 
+    owner => root, 
+    group => root, 
+    alias => "passenger_load", 
+    notify => Service["apache2"], 
+    source => "puppet:///modules/passenger/passenger.load", 
+    before => Exec["enable_passenger"] 
+  }   
+  exec { "a2enmod passenger": 
+    user => root, 
+    alias => "enable_passenger" 
+  }
+  
   #notice("Installing Passenger done. ")
 }
 
